7a15f79f967b124c8e5c989ee562ae442c32f862
Julian Bolanos <julian.camilob24@gmail.com>
2021-02-12 15:16:45 -0500

    Classes created: Main and ClassroomGUI in package ui

---
+++ src/ui/ClassroomGUI.java
@@ -0,0 +1,13 @@
+package ui;
+
+import model.Classroom;
+
+public class ClassroomGUI {
+	
+	private Classroom classroom;
+	
+	public ClassroomGUI(Classroom cr) {
+		classroom = cr;
+	}
+
+}
---
+++ src/ui/Main.java
@@ -0,0 +1,15 @@
+package ui;
+
+import model.Classroom;
+
+public class Main {
+
+	private ClassroomGUI classroomgui;
+	private Classroom classroom;
+	
+	public static void main(String[] args) {
+
+	}
+	
+
+}
