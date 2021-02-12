cda6a87fea02e4fbfe2a89b607b6ddd62a2df84c
Julian Bolanos <julian.camilob24@gmail.com>
2021-02-12 15:49:34 -0500

    Fields added to the class UserAccount in package model

--- src/model/UserAccount.java
+++ src/model/UserAccount.java
@@ -1,5 +1,19 @@
 package model;
 
+import java.io.File;
+
 public class UserAccount {
+	
+	private String userName;
+	private String password;
+	
+	private File photo;
+	
+	//private Gender gender;
+	//private Arraylist<Career> carrers;
+	
+	private String birthday;
+	// private Browser favBrowser;
+	
 
 }
