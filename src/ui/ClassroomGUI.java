89a84f8d961a36867b670b961ba5366c0ad75319
Julian Bolanos <julian.camilob24@gmail.com>
2021-02-12 20:52:56 -0500

    jx:id's added in the fxmls and to the controller class as private fields, also methods (unimplemented) related on actions

--- src/ui/ClassroomGUI.java
+++ src/ui/ClassroomGUI.java
@@ -1,13 +1,69 @@
 package ui;
 
+import com.jfoenix.controls.JFXTextField;
+import com.jfoenix.controls.JFXDatePicker;
+import com.jfoenix.controls.JFXPasswordField;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.Pane;
+import javafx.scene.control.ChoiceBox;
 import model.Classroom;
 
 public class ClassroomGUI {
 	
 	private Classroom classroom;
 	
+	@FXML
+    private Pane mainPane;
+	
+	//login
+	@FXML
+    private JFXTextField txtUserName;
+
+    @FXML
+    private JFXTextField txtPassword;
+    
+    //register
+    @FXML
+    private JFXTextField txtUsernameSignUp;
+
+    @FXML
+    private JFXTextField txtFile;
+
+    @FXML
+    private JFXPasswordField txtPasswordSignUp;
+
+    @FXML
+    private JFXDatePicker txtDateSignUp;
+
+    @FXML
+    private ChoiceBox<?> txtBrowserChoice;
+	
 	public ClassroomGUI(Classroom cr) {
 		classroom = cr;
 	}
+	
+	//login methods
+	@FXML
+	public void logIn(ActionEvent event) {
+
+    }
+
+    @FXML
+	public void showSignUp(ActionEvent event) {
+	
+    }
+    
+    //register methods
+    @FXML
+    public void createAccount(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void showLogIn(ActionEvent event) {
+
+    }
 
 }
