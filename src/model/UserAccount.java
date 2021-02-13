729d9b3e2a473ebb5f03909dfa3da19d13738f8d
Julian Bolanos <julian.camilob24@gmail.com>
2021-02-12 20:08:22 -0500

    Constructor method, getters and setters of the class UserAccount added

--- src/model/UserAccount.java
+++ src/model/UserAccount.java
@@ -1,6 +1,7 @@
 package model;
 
 import java.io.File;
+import java.util.ArrayList;
 
 public class UserAccount {
 	
@@ -9,11 +10,80 @@ public class UserAccount {
 	
 	private File photo;
 	
-	//private Gender gender;
-	//private Arraylist<Career> carrers;
+	private String gender;
+	private ArrayList<String> carrers;
 	
 	private String birthday;
-	// private Browser favBrowser;
+	private String favBrowser;
+	
+	public UserAccount( String usrName, String passwd, String pathPhoto, 
+						String gnder, ArrayList<String> carrs, String bthday,
+						String favBrwser) {
+		
+		setUserName(usrName);
+		setPassword(passwd);
+		setPhoto(pathPhoto);
+		setGender(gnder);
+		setCarrers(carrs);
+		setBirthday(bthday);
+		setFavBrowser(favBrwser);
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public File getPhoto() {
+		return photo;
+	}
+
+	public void setPhoto(String pathPhoto) {
+		this.photo = new File(pathPhoto);
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public ArrayList<String> getCarrers() {
+		return carrers;
+	}
+
+	public void setCarrers(ArrayList<String> carrers) {
+		this.carrers = carrers;
+	}
+
+	public String getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+
+	public String getFavBrowser() {
+		return favBrowser;
+	}
+
+	public void setFavBrowser(String favBrowser) {
+		this.favBrowser = favBrowser;
+	}
 	
 
 }
