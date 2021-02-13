94cf73d1d9fbe50947a2ef84c65f5113e1917937
Julian Bolanos <julian.camilob24@gmail.com>
2021-02-12 20:15:14 -0500

    Constructor method, getters and setters of the class Classroom added

--- src/model/Classroom.java
+++ src/model/Classroom.java
@@ -6,4 +6,16 @@ public class Classroom {
 
 	private ArrayList<UserAccount> accounts;
 	
+	public Classroom() {
+		setAccounts(new ArrayList<UserAccount>());
+	}
+
+	public ArrayList<UserAccount> getAccounts() {
+		return accounts;
+	}
+
+	public void setAccounts(ArrayList<UserAccount> accounts) {
+		this.accounts = accounts;
+	}
+	
 }
